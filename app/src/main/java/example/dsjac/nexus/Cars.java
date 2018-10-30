c3db35af85e11266d1ff288511932e88702981db
Darius Jack <dsjack17@gmail.com>
2018-10-30 13:34:07 -0500

    created initial project

---
+++ app/src/main/java/example/dsjac/nexus/Cars.java
@@ -0,0 +1,13 @@
+package example.dsjac.nexus;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Cars extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cars);
+    }
+}
