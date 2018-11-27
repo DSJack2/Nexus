10f147a06cf821270f1e7ede866dd58da93c1005
Darius Jack <dsjack17@gmail.com>
2018-11-27 13:14:58 -0600

    attempts to have uber button fetch ride info

--- app/src/main/java/example/dsjac/nexus/Cars.java
+++ app/src/main/java/example/dsjac/nexus/Cars.java
@@ -12,6 +12,7 @@ import com.lyft.lyftbutton.RideParams;
 import com.lyft.lyftbutton.RideTypeEnum;
 import com.lyft.networking.ApiConfig;
 import com.uber.sdk.android.core.UberSdk;
+import com.uber.sdk.android.rides.RideRequestButtonCallback;
 import com.uber.sdk.rides.client.ServerTokenSession;
 import com.uber.sdk.android.rides.RideRequestButton;
 import com.uber.sdk.android.rides.RideParameters;
@@ -20,6 +21,7 @@ import com.uber.sdk.rides.client.SessionConfiguration;
 import com.uber.sdk.rides.*;
 
 import com.lyft.networking.ApiConfig;
+import com.uber.sdk.rides.client.error.ApiError;
 
 import java.util.Arrays;
 
@@ -29,16 +31,6 @@ public class Cars extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cars);
-
-        // Uber API Config
-        SessionConfiguration config = new SessionConfiguration.Builder()
-                .setClientId("xxxxx")
-                .setServerToken("xxxxx")
-//                .setRedirectUri("http://localhost")
-                .setScopes(Arrays.asList(Scope.RIDE_WIDGETS))
-                .setEnvironment(SessionConfiguration.Environment.PRODUCTION)
-                .build();
-        UberSdk.initialize(config);
 
         // Create the ride request object to be used on Cars Activity
         RideRequestButton uberRequestButton = new RideRequestButton(Cars.this);
@@ -50,21 +42,49 @@ public class Cars extends AppCompatActivity {
                 // Optional product_id from /v1/products endpoint (e.g. UberX). If not provided, most cost-efficient product will be used
                 .setProductId("a1111c8c-c720-46c3-8534-2fcdd730040d")
                 // Required for price estimates; lat (Double), lng (Double), nickname (String), formatted address (String) of dropoff location
-                .setDropoffLocation(
-                        37.775304, -122.417522, "Uber HQ", "1455 Market Street, San Francisco")
+                .setDropoffLocation(37.775304, -122.417522, "Uber HQ", "1455 Market Street, San Francisco")
                 // Required for pickup estimates; lat (Double), lng (Double), nickname (String), formatted address (String) of pickup location
                 .setPickupLocation(37.775304, -122.417522, "Uber HQ", "1455 Market Street, San Francisco")
                 .build();
 
+        // Uber API Config
+        SessionConfiguration uberConfig = new SessionConfiguration.Builder()
+                .setClientId("ZWBGzmZHqvaCQXK3Tf2UVc3ijY3iyOoH")
+                .setServerToken("ExQMNvmuTdVGFEs9eVdVUzTO25IwkycKqG9KP7tP")
+//                .setRedirectUri("http://localhost")
+                .setScopes(Arrays.asList(Scope.RIDE_WIDGETS))
+                .setEnvironment(SessionConfiguration.Environment.SANDBOX)
+                .build();
+        UberSdk.initialize(uberConfig);
+        ServerTokenSession uberSession = new ServerTokenSession(uberConfig);
+
+        RideRequestButtonCallback uberCallback = new RideRequestButtonCallback() {
+            @Override
+            public void onRideInformationLoaded() {
+
+            }
+
+            @Override
+            public void onError(ApiError apiError) {
+
+            }
+
+            @Override
+            public void onError(Throwable throwable) {
+
+            }
+        };
+
         uberRequestButton.setRideParameters(rideParams);
-        ServerTokenSession session = new ServerTokenSession(config);
-        uberRequestButton.setSession(session);
+        uberRequestButton.setSession(uberSession);
+        uberRequestButton.setCallback(uberCallback);
         uberRequestButton.loadRideInformation();
+
 
         // Lyft API Config
         ApiConfig lyftApiConfig = new ApiConfig.Builder()
-                .setClientId("xxxxx")
-                .setClientToken("xxxx")
+                .setClientId("uG3ayZblOqHr")
+                .setClientToken("XiJQCYnZR6qhvPsnyMbOyrgIQB2esrddDmBwSIVZbDO9fRriljuBKQaIg3NZOI2ssf6XsGq4E/K07tOUyG0QZQQEBY4kOSoMsGg90lYWXd+V1H8yAfB8llc=")
                 .build();
 
         LyftButton lyftRequestButton = findViewById(R.id.lyft_button);
