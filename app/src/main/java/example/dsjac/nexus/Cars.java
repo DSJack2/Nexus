cce102216c8b3c63aeed7eeae9eef8aa3de0ee14
Darius Jack <dsjack17@gmail.com>
2018-11-29 10:51:25 -0600

    initial ec2 http request setup

--- app/src/main/java/example/dsjac/nexus/Cars.java
+++ app/src/main/java/example/dsjac/nexus/Cars.java
@@ -1,10 +1,16 @@
 package example.dsjac.nexus;
 
+import android.preference.PreferenceActivity;
 import android.support.constraint.ConstraintLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.app.Activity;
 import android.widget.RelativeLayout;
+import example.dsjac.nexus.NexusRestClient;
+
+import org.json.*;
+import com.loopj.android.http.*;
+import org.apache.http.Header;
 
 import com.lyft.lyftbutton.LyftButton;
 import com.lyft.lyftbutton.LyftStyle;
@@ -12,6 +18,7 @@ import com.lyft.lyftbutton.RideParams;
 import com.lyft.lyftbutton.RideTypeEnum;
 import com.lyft.networking.ApiConfig;
 import com.uber.sdk.android.core.UberSdk;
+import com.uber.sdk.android.core.auth.AccessTokenManager;
 import com.uber.sdk.android.rides.RideRequestButtonCallback;
 import com.uber.sdk.rides.client.ServerTokenSession;
 import com.uber.sdk.android.rides.RideRequestButton;
@@ -32,6 +39,16 @@ public class Cars extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cars);
 
+        // Uber API Config
+        SessionConfiguration uberConfig = new SessionConfiguration.Builder()
+            .setClientId("xxxxxx")
+            .setServerToken("xxxxx")
+            .setScopes(Arrays.asList(Scope.RIDE_WIDGETS))
+            .setEnvironment(SessionConfiguration.Environment.SANDBOX)
+            .build();
+        UberSdk.initialize(uberConfig);
+        ServerTokenSession uberSession = new ServerTokenSession(uberConfig);
+
         // Create the ride request object to be used on Cars Activity
         RideRequestButton uberRequestButton = new RideRequestButton(Cars.this);
         ConstraintLayout layout = new ConstraintLayout(this);
@@ -39,52 +56,28 @@ public class Cars extends AppCompatActivity {
 
         // set parameters for the uber ride button
         RideParameters rideParams = new RideParameters.Builder()
-                // Optional product_id from /v1/products endpoint (e.g. UberX). If not provided, most cost-efficient product will be used
-                .setProductId("a1111c8c-c720-46c3-8534-2fcdd730040d")
-                // Required for price estimates; lat (Double), lng (Double), nickname (String), formatted address (String) of dropoff location
-                .setDropoffLocation(37.775304, -122.417522, "Uber HQ", "1455 Market Street, San Francisco")
-                // Required for pickup estimates; lat (Double), lng (Double), nickname (String), formatted address (String) of pickup location
-                .setPickupLocation(37.775304, -122.417522, "Uber HQ", "1455 Market Street, San Francisco")
-                .build();
-
-        // Uber API Config
-        SessionConfiguration uberConfig = new SessionConfiguration.Builder()
-                .setClientId("ZWBGzmZHqvaCQXK3Tf2UVc3ijY3iyOoH")
-                .setServerToken("ExQMNvmuTdVGFEs9eVdVUzTO25IwkycKqG9KP7tP")
-//                .setRedirectUri("http://localhost")
-                .setScopes(Arrays.asList(Scope.RIDE_WIDGETS))
-                .setEnvironment(SessionConfiguration.Environment.SANDBOX)
-                .build();
-        UberSdk.initialize(uberConfig);
-        ServerTokenSession uberSession = new ServerTokenSession(uberConfig);
-
-        RideRequestButtonCallback uberCallback = new RideRequestButtonCallback() {
-            @Override
-            public void onRideInformationLoaded() {
-
-            }
-
-            @Override
-            public void onError(ApiError apiError) {
-
-            }
-
-            @Override
-            public void onError(Throwable throwable) {
-
-            }
-        };
+            // Optional product_id from /v1/products endpoint (e.g. UberX). If not provided, most cost-efficient product will be used
+            .setProductId("a1111c8c-c720-46c3-8534-2fcdd730040d")
+            // Required for price estimates; lat (Double), lng (Double), nickname (String), formatted address (String) of dropoff location
+            .setDropoffLocation(37.775304, -122.417522, "Uber HQ", "1455 Market Street, San Francisco")
+            // Required for pickup estimates; lat (Double), lng (Double), nickname (String), formatted address (String) of pickup location
+            .setPickupLocation(37.775304, -122.417522, "Uber HQ", "1455 Market Street, San Francisco")
+            .build();
 
         uberRequestButton.setRideParameters(rideParams);
         uberRequestButton.setSession(uberSession);
-        uberRequestButton.setCallback(uberCallback);
+
+        // Make request for ride information
+        getUberEstimate(37.775304, -122.417522, 37.759234, -122.4135125);
         uberRequestButton.loadRideInformation();
+
+
 
 
         // Lyft API Config
         ApiConfig lyftApiConfig = new ApiConfig.Builder()
-                .setClientId("uG3ayZblOqHr")
-                .setClientToken("XiJQCYnZR6qhvPsnyMbOyrgIQB2esrddDmBwSIVZbDO9fRriljuBKQaIg3NZOI2ssf6XsGq4E/K07tOUyG0QZQQEBY4kOSoMsGg90lYWXd+V1H8yAfB8llc=")
+                .setClientId("xxxxxx")
+                .setClientToken("xxxxxx")
                 .build();
 
         LyftButton lyftRequestButton = findViewById(R.id.lyft_button);
@@ -92,11 +85,33 @@ public class Cars extends AppCompatActivity {
         lyftRequestButton.setLyftStyle(LyftStyle.HOT_PINK);
 
         RideParams.Builder rideParamsBuilder = new RideParams.Builder()
-                .setPickupLocation(37.7766048, -122.3943629)
-                .setDropoffLocation(37.759234, -122.4135125);
+            .setPickupLocation(37.775304, -122.417522)
+            .setDropoffLocation(37.759234, -122.4135125);
         rideParamsBuilder.setRideTypeEnum(RideTypeEnum.CLASSIC);
 
         lyftRequestButton.setRideParams(rideParamsBuilder.build());
         lyftRequestButton.load();
+    }
+
+    private void getUberEstimate(double startLatitude, double startLongitude, double endLatitude, double endLongitude){
+        NexusRestClient client = new NexusRestClient();
+        RequestParams params = new RequestParams();
+        params.put("start_latitude", startLatitude);
+        params.put("start_longitude", startLongitude);
+        params.put("end_latitude", endLatitude);
+        params.put("end_latitude", endLongitude);
+
+        client.get("api/uberPriceEstimate", params, new JsonHttpResponseHandler() {
+
+//            @Override
+//            public void onSuccess(int statusCode, Header[] headers, JSONObject repsonse){
+//
+//            }
+//
+//            @Override
+//            public void onSuccess(int statusCode, Header[] headers, JSONArray prices) {
+//
+//            }
+        });
     }
 }
