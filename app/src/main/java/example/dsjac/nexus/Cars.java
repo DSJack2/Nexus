d843b7e3921890aabfd71cb5c3ead7561cd7bfc5
gwoldy8 <grum.b.woldemichael@vanderbilt.edu>
2018-12-04 00:11:49 -0600

    Dynamically finds current location and requests destination while then searching

--- app/src/main/java/example/dsjac/nexus/Cars.java
+++ app/src/main/java/example/dsjac/nexus/Cars.java
@@ -1,11 +1,32 @@
 package example.dsjac.nexus;
 
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.location.Criteria;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Build;
+import android.support.annotation.NonNull;
 import android.support.constraint.ConstraintLayout;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.app.Activity;
+import android.util.Log;
+import android.view.Window;
 import android.widget.RelativeLayout;
 
+import com.google.android.gms.common.api.Status;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.location.places.Place;
+import com.google.android.gms.location.places.ui.PlaceAutocompleteFragment;
+import com.google.android.gms.location.places.ui.PlaceSelectionListener;
+import com.google.android.gms.maps.CameraUpdate;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
 import com.lyft.lyftbutton.LyftButton;
 import com.lyft.lyftbutton.LyftStyle;
 import com.lyft.lyftbutton.RideParams;
@@ -23,6 +44,7 @@ import com.lyft.networking.ApiConfig;
 
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -31,25 +53,142 @@ import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
+import com.google.android.gms.location.FusedLocationProviderClient;
+
 import java.util.Arrays;
 
-public class Cars extends AppCompatActivity implements OnMapReadyCallback {
+import static android.Manifest.permission.ACCESS_FINE_LOCATION;
+
+public class Cars extends AppCompatActivity implements
+               OnMapReadyCallback,
+               ActivityCompat.OnRequestPermissionsResultCallback {
+
+    private GoogleMap mMap;
+    private static double currentLatitude;
+    private static double currentLongitude;
+    private FusedLocationProviderClient client;
+    private static double destinationLatitude;
+    private static double destinationLongitude;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cars);
 
-        // Get the SupportMapFragment and request notification
-        // when the map is ready to be used.
+        // Get the SupportMapFragment and request notification when the map is ready to be used.
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+    }
+
+    /**
+     * Manipulates the map once available.
+     * This callback is triggered when the map is ready to be used.
+     * This is where we can add markers or lines, add listeners or move the camera. In this case,
+     * we just add a marker near Sydney, Australia.
+     * If Google Play services is not installed on the device, the user will be prompted to install
+     * it inside the SupportMapFragment. This method will only be triggered once the user has
+     * installed Google Play services and returned to the app.
+     */
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+
+        mMap = googleMap;
+
+        requestPermission();
+        client = LocationServices.getFusedLocationProviderClient(this);
+
+
+        if(ActivityCompat.checkSelfPermission(Cars.this, Manifest.permission.ACCESS_FINE_LOCATION) > 0){
+            return;
+        }
+
+        client.getLastLocation().addOnSuccessListener(Cars.this, new OnSuccessListener<Location>() {
+            @Override
+            public void onSuccess(Location location) {
+                if(location != null){
+                    updateLocation(location);
+                    LatLng currentLocation = new LatLng(location.getLatitude(), location.getLongitude());
+                    mMap.addMarker(new MarkerOptions().position(currentLocation)
+                            .title("Current Location"));
+                    mMap.moveCamera(CameraUpdateFactory.newLatLng(currentLocation));
+                    mMap.moveCamera(CameraUpdateFactory.zoomTo(15f));
+
+                    PlaceAutocompleteFragment autocompleteFragment = (PlaceAutocompleteFragment)
+                            getFragmentManager().findFragmentById(R.id.place_autocomplete_fragment);
+
+                    autocompleteFragment.setOnPlaceSelectedListener(new PlaceSelectionListener() {
+                        @Override
+                        public void onPlaceSelected(Place place) {
+                            updateDestination(place);
+                            mMap.addMarker(new MarkerOptions().position(place.getLatLng())
+                                    .title("Final Destination"));
+                            mMap.moveCamera(CameraUpdateFactory.newLatLng(place.getLatLng()));
+                            mMap.moveCamera(CameraUpdateFactory.zoomIn());
+                            configureLyft(place);
+                            configureUber(place);
+                        }
+                        @Override
+                        public void onError(Status status) {
+                            // TODO: Handle the error.
+                            Log.i("TAG", "An error occurred: " + status);
+                        }
+                    });
+                }
+            }
+        });
+    }
+
+    private void requestPermission(){
+        ActivityCompat.requestPermissions(this, new String[]{ACCESS_FINE_LOCATION}, 1);
+    }
+
+    public void updateLocation(Location location){
+        currentLatitude = location.getLatitude();
+        currentLongitude = location.getLongitude();
+    }
+
+    public void updateDestination(Place place){
+        LatLng destination = place.getLatLng();
+        destinationLatitude = destination.latitude;
+        destinationLongitude = destination.longitude;
+    }
+
+    public void configureLyft(Place place){
+
+        LatLng destination = place.getLatLng();
+        destinationLatitude = destination.latitude;
+        destinationLongitude = destination.longitude;
+
+        ApiConfig lyftApiConfig = new ApiConfig.Builder()
+                .setClientId("uG3ayZblOqHr")
+                .setClientToken("XiJQCYnZR6qhvPsnyMbOyrgIQB2esrddDmBwSIVZbDO9fRriljuBKQaIg3NZOI2ssf6XsGq4E/K07tOUyG0QZQQEBY4kOSoMsGg90lYWXd+V1H8yAfB8llc=")
+                .build();
+
+        LyftButton lyftRequestButton = findViewById(R.id.lyft_button);
+        lyftRequestButton.setApiConfig(lyftApiConfig);
+        lyftRequestButton.setLyftStyle(LyftStyle.HOT_PINK);
+
+        RideParams.Builder rideParamsBuilder = new RideParams.Builder()
+                .setPickupLocation(currentLatitude, currentLongitude)
+                .setDropoffLocation(destinationLatitude, destinationLongitude);
+        rideParamsBuilder.setRideTypeEnum(RideTypeEnum.CLASSIC);
+
+        lyftRequestButton.setRideParams(rideParamsBuilder.build());
+        lyftRequestButton.load();
+    }
+
+    public void configureUber(Place place){
+
+        LatLng destination = place.getLatLng();
+        destinationLatitude = destination.latitude;
+        destinationLongitude = destination.longitude;
 
         // Uber API Config
         SessionConfiguration config = new SessionConfiguration.Builder()
-                .setClientId("xxxxx")
-                .setServerToken("xxxxx")
+                .setClientId("ZWBGzmZHqvaCQXK3Tf2UVc3ijY3iyOoH")
+                .setServerToken("ExQMNvmuTdVGFEs9eVdVUzTO25IwkycKqG9KP7tP")
 //                .setRedirectUri("http://localhost")
                 .setScopes(Arrays.asList(Scope.RIDE_WIDGETS))
                 .setEnvironment(SessionConfiguration.Environment.PRODUCTION)
@@ -76,46 +215,6 @@ public class Cars extends AppCompatActivity implements OnMapReadyCallback {
         ServerTokenSession session = new ServerTokenSession(config);
         uberRequestButton.setSession(session);
         uberRequestButton.loadRideInformation();
-
-        // Lyft API Config
-        ApiConfig lyftApiConfig = new ApiConfig.Builder()
-                .setClientId("xxxxx")
-                .setClientToken("xxxx")
-                .build();
-
-        LyftButton lyftRequestButton = findViewById(R.id.lyft_button);
-        lyftRequestButton.setApiConfig(lyftApiConfig);
-        lyftRequestButton.setLyftStyle(LyftStyle.HOT_PINK);
-
-        RideParams.Builder rideParamsBuilder = new RideParams.Builder()
-                .setPickupLocation(36.1447, -86.8027)
-                .setDropoffLocation(36.1668, -86.8276);
-        rideParamsBuilder.setRideTypeEnum(RideTypeEnum.CLASSIC);
-
-        lyftRequestButton.setRideParams(rideParamsBuilder.build());
-        lyftRequestButton.load();
     }
 
-    /**
-     * Manipulates the map once available.
-     * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
-     * If Google Play services is not installed on the device, the user will be prompted to install
-     * it inside the SupportMapFragment. This method will only be triggered once the user has
-     * installed Google Play services and returned to the app.
-     */
-    @Override
-    public void onMapReady(GoogleMap googleMap) {
-        // Add a marker in Sydney, Australia,
-        // and move the map's camera to the same location.
-        LatLng vandy = new LatLng(36.1447, -86.8027);
-        LatLng tsu = new LatLng(36.1668, -86.8276);
-        googleMap.addMarker(new MarkerOptions().position(vandy)
-                .title("Marker at Vandy"));
-        googleMap.addMarker(new MarkerOptions().position(tsu)
-                .title("Marker at TSU"));
-        googleMap.moveCamera(CameraUpdateFactory.newLatLng(vandy));
-        googleMap.animateCamera( CameraUpdateFactory.zoomTo( 12.0f ) );
-    }
 }
